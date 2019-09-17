934d30849aadadc6435489d5415fea0715d33900
Anselm R. Garbe <anselm@garbe.ca>
2019-09-17 15:16:26 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# staw
-Simple static web generator similar to swerc written in Go
+staw
+====
+Simple *stat*ic *w*eb generator, similar to (s)werc, written in Go. In contrast
+to werc it generates static web sites that don't require (fast)cgi execution on
+demand.
+
+staw supports a very simple migration from existing werc sites to static staw sites.
+
+staw creates HTML output for each directory and markdown input file. It also
+copies all other files to the output directory. The output directory should be
+served as docroot for the particular site by a regular web server.
+
+staw uses Go's template approach and the
+[github.com/gomarkdown/markdown](https://github.com/gomarkdown/markdown)
+markdown package.  staw is self-contained and does not require any other
+dependencies.
+
+Usage
+-----
+	; staw -h
+	Usage of ./staw:
+	  -css string
+		style.css file to be copied to site output directory (optional)
+	  -in string
+		input site directory (required)
+	  -out string
+		output site directory (required)
+	  -p string
+		url-prefix for local testing (optional)
+	  -t string
+		site title of the site (required)
+	  -tpl string
+		template file to be used (required) (default "default.tpl")
+
+site directory
+--------------
+staw uses a site directory structure similar to the one introduced by werc.
+Each site lives in its directory tree.
+
+Example:
+
+	example.com
+	   +- index.md
+	   +- some-topic
+	      +- intro.md
+
+	; staw -p file:///home/anselm/www/ -tpl default.tpl -css style.css \
+		-t 'example' -in /home/anselm/sites/example.com -out /home/anselm/www/example.com
+
+This run will generate:
+
+	example.com
+	   +- style.css
+	   +- index.html
+	   +- some-topic
+	      +- index.html
+	      +- intro
+                 +- index.html
+
+Template
+--------
+Have a look at the default.tpl provided by staw.
+
+staw uses a recursive menu structure and defines the following variables that
+can be used during the template execution on each markdown page:
+
+	.Site string
+	.SiteTitle string
+	.Prefix string
+	.Title string
+	.HtmlContent string
+	range .Items {
+		.Prefix string
+		.Path string
+		.Name string
+		.Sel bool
+		range .Items ...
+	}
+
+Happy hacking.
---
+++ staw.go
@@ -0,0 +1,193 @@
+// See LICENSE file for copyright and license details.
+package main
+
+import (
+	"bufio"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+	"github.com/gomarkdown/markdown"
+)
+
+type Args struct {
+	siteDir string
+	srcPath string
+	dstPath string
+	tpl string
+	page Page
+}
+
+type Menu struct {
+	Prefix string
+	Path string
+	Name string
+	Sel bool
+	Items []Menu
+}
+
+type Page struct {
+	Site string
+	SiteTitle string
+	Prefix string
+	Title string
+	HtmlContent string
+	Items []Menu
+}
+
+func buildMenu(cwd, path, prefix string, walk []string) []Menu {
+	files, err := ioutil.ReadDir(cwd)
+	dieOnError(err)
+	var menu []Menu
+	for _, f := range files {
+		sel := len(walk) > 0 && f.Name() == walk[0]
+		if f.IsDir() {
+			m := Menu{prefix, path + f.Name() + "/index.html", f.Name() + "/", sel, nil}
+			if sel {
+				m.Items = buildMenu(cwd + "/" + f.Name(), path + f.Name() + "/", prefix, walk[1:])
+			}
+			menu = append(menu, m)
+		} else if isMdFile(f) {
+			title := getTitle(cwd + "/" + f.Name())
+			tmp := strings.TrimSuffix(f.Name(), ".md")
+			if tmp == "index" {
+				// prepend
+				menu = append([]Menu{Menu{prefix, path + "index.html", title, sel, nil}}, menu...)
+			} else {
+				menu = append(menu, Menu{prefix, path + tmp + "/index.html", title, sel, nil})
+			} 
+		}
+	}
+	return menu
+}
+
+func copyFile(src, dst string) {
+	in, err := os.Open(src)
+	dieOnError(err)
+	defer in.Close()
+	out, err := os.Create(dst)
+	dieOnError(err)
+	defer out.Close()
+	_, err = io.Copy(out, in)
+	dieOnError(err)
+	dieOnError(out.Close())
+}
+
+func dieIfEmpty(s *string, msg string) {
+	if *s == "" {
+		log.Fatal(msg)
+	}
+}
+
+func dieOnError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getDstPath(dstPath string, f os.FileInfo) string {
+	var dst, tmp string
+	if f == nil {
+		tmp = "index" // empty md directory case
+	} else {
+		tmp = strings.TrimSuffix(f.Name(), ".md")
+	}
+	if tmp != "index" {
+		dst = dstPath + "/" + tmp
+		os.Mkdir(dst, os.ModePerm)
+		dst += "/index.html"
+	} else {
+		dst = dstPath + "/index.html"
+	}
+	return dst
+}
+
+func getTitle(src string) string {
+	in, err := os.Open(src)
+	dieOnError(err)
+	defer in.Close()
+	reader := bufio.NewReader(in)
+	title, err := reader.ReadString('\n')
+	dieOnError(err)
+	return strings.TrimRight(title, "\n")
+}
+
+func isMdFile(f os.FileInfo) bool {
+	return strings.HasSuffix(f.Name(), ".md")
+}
+
+func processMdFile(a Args, walk []string, f os.FileInfo) {
+	if f != nil {
+		walk = append(walk, f.Name())
+		a.page.Title = getTitle(a.srcPath)
+		dat, err := ioutil.ReadFile(a.srcPath)
+		dieOnError(err)
+		a.page.HtmlContent = string(markdown.ToHTML(dat, nil, nil))
+	} else if len(walk) > 0 {
+		a.page.Title = walk[len(walk)-1] + "/"
+	}
+	a.page.Items = buildMenu(a.siteDir, "", a.page.Prefix, walk)
+	t, err := template.ParseFiles(a.tpl)
+	dieOnError(err)
+	dst := getDstPath(a.dstPath, f)
+        out, err := os.Create(dst)
+	dieOnError(err)
+        defer out.Close()
+	dieOnError(t.Execute(out, a.page))
+}
+
+func processPath(a Args, walk []string) {
+	os.Mkdir(a.dstPath, os.ModePerm)
+	files, err := ioutil.ReadDir(a.srcPath)
+	dieOnError(err)
+	emptyDir := true
+	for _, f := range files {
+		b := a
+		b.srcPath = a.srcPath + "/" + f.Name()
+	     	if f.IsDir() {
+			b.dstPath = a.dstPath + "/" + f.Name()
+			processPath(b, append(walk, f.Name()))
+	     	} else {
+			if f.Name() == "index.md" {
+				emptyDir = false
+			}
+			if isMdFile(f) {
+				processMdFile(b, walk, f)
+			} else {
+				copyFile(b.srcPath, b.dstPath + "/" + f.Name())
+			}
+	     	}
+	}
+	if emptyDir {
+		processMdFile(a, walk, nil)
+	}
+}
+
+func processSite(a Args, css string) {
+	processPath(a, []string{})
+	if css != "" {
+		f, err := os.Stat(css)
+		dieOnError(err)
+		copyFile(css, a.dstPath + "/" + f.Name())
+	}
+}
+
+func main() {
+	tpl := flag.String("tpl", "default.tpl", "template file to be used (required)")
+	src := flag.String("in", "", "input site directory (required)")
+	dst := flag.String("out", "", "output site directory (required)")
+	title := flag.String("t", "", "site title of the site (required)")
+	prefix := flag.String("p", "", "url-prefix for local testing (optional)")
+	css := flag.String("css", "", "style.css file to be copied to site output directory (optional)")
+	flag.Parse()
+	dieIfEmpty(tpl, "no template given")
+	dieIfEmpty(title, "no site title given")
+	dieIfEmpty(src, "no site input directory given")
+	dieIfEmpty(dst, "no output directory given")
+	site, err := os.Stat(*src)
+	dieOnError(err)
+	processSite(Args{*src, *src, *dst, *tpl, Page{site.Name(), *title, *prefix, "", "", nil}}, *css)
+}

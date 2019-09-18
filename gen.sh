0f3fdbc0d6767ee3e43fd0a4184c9892990244b5
Anselm R. Garbe <anselm@garbe.ca>
2019-09-18 22:18:31 +0000

    Add apostroph to bee site title

--- gen.sh
+++ gen.sh
@@ -2,4 +2,4 @@
 go build
 ./staw -in ../sites/garbe.ca -out /var/www/garbe.ca -t 'Anselm R. Garbe' -css style.css
 ./staw -in ../sites/sta.li -out /var/www/sta.li -t 'static linux' -css style.css
-./staw -in ../sites/beec.ca -out /var/www/beec.ca -t 'Anselms bee research &amp; apiary' -css style.css
+./staw -in ../sites/beec.ca -out /var/www/beec.ca -t 'Anselm&#39;s bee research &amp; apiary' -css style.css

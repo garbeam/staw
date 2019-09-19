88a8dc1d3e9c2e2196b57781a9a6a35b0591c1ba
Anselm R. Garbe <anselm@garbe.ca>
2019-09-19 22:38:33 +0000

    Some fon tweaks

--- default.tpl
+++ default.tpl
@@ -3,6 +3,7 @@
 <head> 
 	<title>{{.Title}} | {{.SiteTitle}}</title>
 	<link rel="stylesheet" type="text/css" href="{{.Prefix}}/style.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat&display=swap">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8"> 
 </head> 
--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 go build
-./staw -in ../sites/garbe.ca -out /var/www/garbe.ca -t 'Anselm R. Garbe' -css style.css
-./staw -in ../sites/sta.li -out /var/www/sta.li -t 'static linux' -css style.css
-./staw -in ../sites/beec.ca -out /var/www/beec.ca -t 'Anselm&#39;s bee research &amp; apiary' -css style.css
+./staw -in ../sites/garbe.ca -out /var/www/static/garbe.ca -t 'Anselm R. Garbe' -css style.css
+./staw -in ../sites/sta.li -out /var/www/static/sta.li -t 'static linux' -css style.css
+./staw -in ../sites/beec.ca -out /var/www/static/beec.ca -t 'Anselm&#39;s bee research &amp; apiary' -css style.css

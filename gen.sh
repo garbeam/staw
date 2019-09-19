932a7a4cd8f381535b51f71563ddee206a5b72cf
Anselm R. Garbe <anselm@garbe.ca>
2019-09-19 09:59:16 -0700

    Revert gen.sh

--- gen.sh
+++ gen.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 go build
-mkdir -p /tmp/www
-./staw -p file:///tmp/www/garbe.ca -in ../sites/garbe.ca -out /tmp/www/garbe.ca -t 'Anselm R. Garbe' -css style.css
-./staw -p file:///tmp/www/sta.li -in ../sites/sta.li -out /tmp/www/sta.li -t 'static linux' -css style.css
-./staw -p file:///tmp/www/beec.ca -in ../sites/beec.ca -out /tmp/www/beec.ca -t 'Anselm&#39;s bee research &amp; apiary' -css style.css
+./staw -in ../sites/garbe.ca -out /var/www/garbe.ca -t 'Anselm R. Garbe' -css style.css
+./staw -in ../sites/sta.li -out /var/www/sta.li -t 'static linux' -css style.css
+./staw -in ../sites/beec.ca -out /var/www/beec.ca -t 'Anselm&#39;s bee research &amp; apiary' -css style.css
